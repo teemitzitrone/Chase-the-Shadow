2a7c5fa00d59eb67b8b1d1b490541bcb3dd16adf
Agata Raap <a.raap@ymail.com>
2013-08-20 21:40:26 +0200

    GameObject

--- ya2drpg/source/engine/GameObject.h
+++ ya2drpg/source/engine/GameObject.h
@@ -1,7 +1,19 @@
 #pragma once
+#include <SDL.h>
+
+#define INPT /*INPUTCLASS*/
+
 class GameObject
 {
 public:
-	GameObject(void);
+	GameObject(void): _alive(true) { /* */ };
 	virtual ~GameObject(void);
+	virtual void StartUp() = 0;
+	virtual void Input(INPT) = 0;
+	virtual void Update() = 0;
+	virtual void Render(SDL_Renderer *renderer) = 0;
+	virtual void ShutDown() = 0;
+
+private:
+	bool _alive;
 };
