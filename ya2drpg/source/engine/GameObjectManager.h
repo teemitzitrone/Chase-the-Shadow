7b1700fe2686bfe0a87112996402e270901d30bf
Maximilian Ratz <maximilian.ratz@gmail.com>
2013-08-20 23:21:16 +0200

    added GameObjectManager

---
+++ ya2drpg/source/engine/GameObjectManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include "GameObject.h"
+
+class GameObjectManager
+{
+	std::vector<GameObject*> _gameobjects;
+
+public:
+	GameObjectManager(void);
+	virtual ~GameObjectManager(void);
+	
+	void RegisterGameobject(GameObject*);
+	void UnregisterGameobject(GameObject*);
+
+
+    typedef std::vector<GameObject*>::iterator iterator;
+    typedef std::vector<GameObject*>::const_iterator const_iterator;
+
+	iterator begin() { return this->_gameobjects.begin(); }
+    const_iterator begin() const { return this->_gameobjects.begin(); }
+
+    iterator end() { return this->_gameobjects.end(); }
+    const_iterator end() const { return this->_gameobjects.end(); }
+};
+
