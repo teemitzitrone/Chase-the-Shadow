cf8c23a432a849456e3cf919f09f565e099de64c
Agata Raap <a.raap@ymail.com>
2013-09-10 17:08:37 +0200

    added _engine project with vector2d class

---
+++ _engine/source/Vector2D.h
@@ -0,0 +1,43 @@
+#ifndef _VECTOR2D_H
+#define _VECTOR2D_H
+#pragma once
+#include <math.h>
+
+namespace engine
+{
+	class Vector2D
+	{
+	public:
+		Vector2D(float x = 0, float y = 0): _x(x), _y(y) {};
+		virtual ~Vector2D() {};
+
+		void Rotate( const float angle );
+		float Magnitude() const;
+		float Normalize();
+		float DotProduct( const Vector2D& v2 ) const;
+		float CrossProduct( const Vector2D& v2 ) const;
+		float Distance(const Vector2D& v2) const;
+		float GetX() const;
+		float GetY() const;
+
+		static Vector2D Zero() { return Vector2D(); };
+
+		Vector2D& operator= ( const Vector2D& v2 );
+		Vector2D& operator+= ( const Vector2D& v2 );
+		Vector2D& operator-= ( const Vector2D& v2 );
+		Vector2D& operator*= ( const float scalar);
+		Vector2D& operator/= ( const float scalar);
+
+		const Vector2D operator+( const Vector2D &v2 ) const;
+		const Vector2D operator-( const Vector2D &v2 ) const;
+		const Vector2D operator*( const float scalar ) const;
+		const Vector2D operator/( const float scalar ) const;
+
+		bool operator== ( const Vector2D& v2 ) const;
+		bool operator!= ( const Vector2D& v2 ) const;
+
+	private:
+		float _x, _y;
+	};
+}
+#endif
